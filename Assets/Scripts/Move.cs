bcffd99cbedc6d104a6da9c003b5768a34536f66
Art21kir <152896031+Art21kir@users.noreply.github.com>
2024-07-12 12:41:01 +0300

    0.03
    Карта + ограничения по движению

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 
 public class Move : MonoBehaviour
 {
-    public GameObject player;
     private Vector3 velocity = Vector3.zero;
     private void Update()
     {
@@ -10,7 +9,22 @@ public class Move : MonoBehaviour
         {
             velocity = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             velocity = new Vector3(velocity.x, velocity.y, 0);
+            if (velocity.x < -8.5f) velocity.x = -8.5f;
+            if (velocity.x > 23.5f) velocity.x = 23.5f;
+            if (velocity.y < -10.5f) velocity.y = -10.5f;
+            if (velocity.x < 1)
+            {
+                if (velocity.y > 5.5f) velocity.y = 5.5f;
+            }
+            else
+            {
+                if (velocity.y > 7)
+                {
+                    velocity.y = 7;
+                }
+                if (velocity.x < 1 && velocity.y > 5.5) velocity.x = 1;
+            }
         }
-        player.transform.position = Vector3.MoveTowards(player.transform.position, velocity, 5f * Time.deltaTime);
+        transform.position = Vector3.MoveTowards(transform.position, velocity, 5f * Time.deltaTime);
     }
 }
