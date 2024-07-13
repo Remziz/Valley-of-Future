0eaf87d066a9a33f78935eb4b2ffe44c708485e9
Art21kir <152896031+Art21kir@users.noreply.github.com>
2024-07-13 12:22:01 +0300

    0.08
    Класс клетка начат

--- Assets/Scripts/Square.cs
+++ Assets/Scripts/Square.cs
@@ -1,18 +1,56 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Square : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public bool UnderWaterPump;
+    public bool plugged;
+    public bool watered;
+    public bool UnderLightLamp;
+    public string Plant;
+    public bool PlantReady;
+    public int state;
+    public float speed;
+    private bool Render;
+    public Sprite Unpluged;
+    public Sprite Pluged;
+    public Sprite Watered;
+    public void tRender()
     {
-        
+        Render = true;
+    }
+    public void ToPlant(string name)
+    {
+        state = 1;
+        Plant = name;
+        tRender();
+    }
+    public void Water()
+    {
+        watered = true;
+        tRender();
+    }
+    public void Collect()
+    {
+        state = 0;
+        plugged = false;
+        PlantReady = false;
+        Plant = "";
+        tRender();
+    }
+    public void Plug()
+    {
+        plugged = true; 
+        tRender(); 
+    }
+    private void Update()
+    {
+        if (Render)
+        {
+            if (watered) gameObject.GetComponent<SpriteRenderer>().sprite = Watered;
+            else if (plugged) gameObject.GetComponent<SpriteRenderer>().sprite = Pluged;
+            else gameObject.GetComponent<SpriteRenderer>().sprite = Unpluged;
+            Render = false;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
